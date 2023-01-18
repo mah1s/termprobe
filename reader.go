4fdbd3e73e5b87b3070d7fa86efa0bd939244358
mahi <15308167+mah1s@users.noreply.github.com>
2023-01-18 07:28:46 +0000

    Plot multiple graphs Changes to plot multiple graphs Concurrent file reads to fetch data

--- reader.go
+++ reader.go
@@ -11,13 +11,18 @@ import (
 	"strings"
 )
 
-func ReadFile(fileToRead string, maxLines int, startPos int64, re string) ([]float64, int64, error) {
+func ReadFile(graphId string, fileToRead string, maxLines int, startPos int64, re string, configName string, ch chan<- DataResp) {
+
+	var resp DataResp
 	var dataFloats []float64
 	r := regexp.MustCompile(re)
 
 	file, err := os.Open(fileToRead)
 	if err != nil {
-		return dataFloats, startPos, err
+		// resp.ConfigName, resp.Data, resp.FileName, resp.Position, resp.Error = configName, dataFloats, fileToRead, startPos, err
+		_, resp.Error = "", err
+		ch <- resp
+		return
 	}
 	defer file.Close()
 
@@ -30,27 +35,33 @@ func ReadFile(fileToRead string, maxLines int, startPos int64, re string) ([]flo
 		if err != nil {
 			// check if file does not exist anymore
 			if _, err := os.Stat(fileToRead); errors.Is(err, os.ErrNotExist) {
-				return dataFloats, startPos, err
+				_, resp.Error = "", err
+				ch <- resp
+				return
 			}
 
-			// other than EOF is a problem
+			// errors other than EOF pose a problem
 			if err != io.EOF {
-				return dataFloats, startPos, err
+				_, resp.Error = "", err
+				ch <- resp
+				return
 			}
 
-			// check if file has reduced in size which could mean file rotation
+			// check if file has reduced in size which could mean file rotation or someone playing with it
 			fileStat, err := file.Stat()
 			if err != nil {
-				return dataFloats, startPos, err
+				_, resp.Error = "", err
+				ch <- resp
+				return
 			}
-
 			// get current file read offset
 			currentRead, err := file.Seek(0, io.SeekCurrent)
 			if err != nil {
-				return dataFloats, startPos, err
+				_, resp.Error = "", err
+				ch <- resp
+				return
 			}
-
-			// if current read offset is greater than total file size,
+			// now if current read offset is greater than total file size,
 			// then start reading from current file size
 			if currentRead > fileStat.Size() {
 				file.Seek(fileStat.Size(), io.SeekStart)
@@ -69,20 +80,23 @@ func ReadFile(fileToRead string, maxLines int, startPos int64, re string) ([]flo
 			}
 
 			if len(matches) > 2 {
-				// future implementation
+				// for future implementation - plot multiple data stream on a single graph
 				log.Fatal("Stopping: regex matched more than one group")
 			}
 
-			// ignore if line wasn't matched (length of matches less than 2)
+			// ignore if line wasn't matched (i.e. length of matches less than 2)
 		}
 	}
 
-	// set startPos to file content read so far
+	// set startPos to file content read so far (for next iteration of read)
 	currentRead, err := file.Seek(0, io.SeekCurrent)
 	if err != nil {
-		return dataFloats, startPos, err
+		_, resp.Error = "", err
+		ch <- resp
+		return
 	}
 	startPos = currentRead
-
-	return dataFloats, startPos, nil
+	// return on channel
+	resp.GraphId, resp.ConfigName, resp.Data, resp.FileName, resp.Position, resp.Error = graphId, configName, dataFloats, fileToRead, startPos, nil
+	ch <- resp
 }
--- termprobe.go
+++ termprobe.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"os"
-	"time"
+	"strconv"
 
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
 	"gopkg.in/yaml.v3"
 )
 
@@ -24,17 +21,8 @@ type Configs struct {
 }
 
 func main() {
-	var title string
-	var fileToRead string
-	var rePattern string
-	var maxLinesToRead int
-	var leftPadding int
-	var topPadding int
-	var rightPadding int
-	var bottomPadding int
 
 	configFile := "config.yml"
-	var startFrom int64 = 0
 
 	yamlFile, err := os.ReadFile(configFile)
 	if err != nil {
@@ -47,87 +35,41 @@ func main() {
 		log.Fatalf("Error while performing Unmarshal: %v", e)
 	}
 
+	graphsData := make(map[string]Graphs)
+
+	i := 0
 	for k, v := range configData {
+		i++
 		fmt.Printf("Plotting: %s[\"%s\"]\n", k, v.Title)
-		title = v.Title
-		fileToRead = v.FilePath
-		rePattern = v.RegexPattern
-		maxLinesToRead = v.MaxLines
-		leftPadding = v.LeftPadding
-		topPadding = v.TopPadding
-		rightPadding = v.RightPadding
-		bottomPadding = v.BottomPadding
-		break // plotting only single graph for now - multiple plots for future
-	}
+		var gm Graphs
 
-	// get file size and set seek to its current size for first time read
-	// program will read from the current position and not from beginning
-	file, err := os.Open(fileToRead)
-	if err != nil {
-		log.Fatalf("failed to read file: %v", err)
-	}
-	fileStat, err := file.Stat()
-	if err != nil {
-		log.Fatalf("failed to get stats of file: %v", err)
-	}
-	startFrom = fileStat.Size()
-	file.Close()
+		// TODO: add checks to verify input
 
-	// initialize termui
-	if err := ui.Init(); err != nil {
-		log.Fatalf("failed to initialize termui: %v", err)
-	}
-	defer ui.Close()
-
-	uiEvents := ui.PollEvents()
-	ticker := time.NewTicker(time.Second).C
-	// give some initial entries for the plot - in case if no data found on first read
-	var plotData = []float64{0, 0}
-
-	drawGraph := func(valsToPlot []float64) {
-		p := widgets.NewPlot()
-		p.Title = title
-		p.Data = make([][]float64, 1)
-		p.Data[0] = valsToPlot
-		p.SetRect(leftPadding, topPadding, rightPadding, bottomPadding)
-		p.TitleStyle.Fg = ui.ColorRed
-		p.AxesColor = ui.ColorWhite
-		p.LineColors[0] = ui.ColorGreen
-		p.PlotType = widgets.LineChart
-
-		ui.Render(p)
-	}
-
-	for {
-		select {
-		case e := <-uiEvents:
-			switch e.ID {
-			case "q", "<C-c>":
-				return
-			}
-		case <-ticker:
-			// read from file
-			data, currentPos, err := ReadFile(fileToRead, maxLinesToRead, startFrom, rePattern)
-			// set startFrom for next iteration of read
-			startFrom = currentPos
-			if errors.Is(err, os.ErrNotExist) {
-				// TODO: ignore if file does not exist, for now
-			} else if err != nil {
-				log.Fatalf("Error: %v", err)
-			}
-
-			// append data for plotting if matches were found
-			if len(data) > 0 {
-				for i := 0; i < len(data); i++ {
-					plotData = append(plotData, data[i])
-				}
-				// reduce data size by removing old data to fit into the plot area for visibility
-				if len(plotData) > rightPadding-10 {
-					plotData = plotData[len(plotData)-(rightPadding-10):]
-				}
-			}
-
-			drawGraph(plotData)
+		// get file size and set seek to its current size for first time read
+		// program will read from the current position and not from beginning
+		file, err := os.Open(v.FilePath)
+		if err != nil {
+			log.Fatalf("failed to read file: %v", err)
 		}
+		fileStat, err := file.Stat()
+		if err != nil {
+			log.Fatalf("failed to get stats of file: %v", err)
+		}
+
+		gm.GraphId = "graph" + strconv.Itoa(i)
+		gm.ConfigName = k
+		gm.Title = v.Title
+		gm.StartFrom = fileStat.Size()
+		gm.Values = []float64{0, 0}
+		gm.LeftPadding = v.LeftPadding
+		gm.TopPadding = v.TopPadding
+		gm.RightPadding = v.RightPadding
+		gm.BottomPadding = v.BottomPadding
+		graphsData[k] = gm
+
+		file.Close()
 	}
+
+	DrawGraph(configData, graphsData)
+
 }
